357e187f83bdd7a04bc0af20ab834cd9dc1e747e
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-24 05:22:16 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,5 +41,7 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-  
+  biological_life = {
+    kingdom: {}
+  }
 end
